bf8240854bdfc3ca2cde9b6ea346754e47f3a93f
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-14 17:17:19 +0100

    [ADD] fileWriter

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,17 +1,20 @@
 package org.lessons.java;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        final String FILE_PATH = "./books.txt";
         System.out.print("Quanti libri vorresti aggiungere? ");
         int n = Integer.parseInt(scanner.nextLine());
         Book[] bookList = new Book[n];
 
         for (int i = 0; i < n; i++) {
-            System.out.print("Inserisci il titolo del libro: ");
+            System.out.print("Inserisci il titolo del " + (i + 1) +  " libro: ");
             String title = scanner.nextLine();
             System.out.print("Inserisci il numero di pagine: ");
             int pages = Integer.parseInt(scanner.nextLine());
@@ -19,6 +22,8 @@ public class Main {
             String author = scanner.nextLine();
             System.out.print("Inserisci l'editore del libro: ");
             String publisher = scanner.nextLine();
+
+
 
             try {
                 Book book = new Book(title, pages, author, publisher);
@@ -28,6 +33,31 @@ public class Main {
                 System.out.println(e.getMessage());
             }
         }
+            scanner.close();
+            System.out.println("Ecco il tuo catalogo dei libri:");
+            System.out.println("*******************");
+            System.out.println(Arrays.toString(bookList));
+            System.out.println("*******************");
 
+            // FILE WRITER
+
+            FileWriter writer = null;
+            try{
+                writer = new FileWriter(FILE_PATH, true);
+                for (Book currentBook : bookList){
+                    writer.write(currentBook.toString() + "/n");
+                }
+            } catch (IOException e){
+                System.out.println("Non è stato possibile creare il file");
+                System.out.println(e.getMessage());
+            } finally {
+                if (writer != null){
+                    try{
+                        writer.close();
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
     }
 }
