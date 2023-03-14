89546c6435bf362dd97f46a8752b4e20fd25b88c
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-14 15:25:43 +0100

    [ADD] question to user

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,7 +1,30 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quanti libri vorresti aggiungere? ");
+        int n = Integer.parseInt(scanner.nextLine());
+        Book[] bookList = new Book[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Inserisci il titolo del libro: ");
+            String title = scanner.nextLine();
+            System.out.print("Inserisci il numero di pagine: ");
+            int pages = Integer.parseInt(scanner.nextLine());
+            System.out.print("Inserisci l'autore del libro: ");
+            String author = scanner.nextLine();
+            System.out.print("Inserisci l'editore del libro: ");
+            String publisher = scanner.nextLine();
+
+            Book book = new Book(title, pages, author, publisher);
+
+            bookList[i] = book;
+            System.out.println(bookList[i]);
+        }
 
     }
 }
